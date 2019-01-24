d6a5b9e40aac4f4e4e10eff90d39dff6bedbc40f
EC2 Default User <ec2-user@ip-172-31-29-186.ec2.internal>
2019-01-24 15:06:56 +0000

    〜 bmi

--- main.js
+++ main.js
@@ -1,6 +1,70 @@
 var app = new Vue({
   el: "#example",
   data: {
-    greeting: "Hello Vue.js!"
+    url: "https://techacademy.jp/"
+  }
+});
+var app2 = new Vue({
+  el:"#example2",
+  data:{
+    name: "太郎"
+  }
+});
+var app3 = new Vue({
+  el:"#example3",
+  data:{
+    greeting: "Hello Vue.js!2"
+  }
+});
+var app4 = new Vue({
+  el:"#example4",
+  data:{
+    members: ["桃太郎", "イヌ", "サル", "キジ"]
+  }
+});
+var app5 = new Vue({
+  el:"#example5",
+  data:{
+    characters:{
+      hero: "桃太郎",
+      friend: "イヌ",
+      enemy: "鬼"
+    }
+  }
+});
+var app6 = new Vue({
+  el: "#example6",
+  data: {
+    name: "太郎"
+  }
+});
+var app7 = new Vue({
+  el: "#example7",
+  data:{
+    height: "",
+    weight: ""
+  }
+});
+var app8 = new Vue({
+  el: "#example8",
+  data:{
+    height:"",
+    weight:""
+  },
+  computed:{
+    bmi: function(){
+      if(this.height && this.weight){
+        //センチメートルをメートルにする
+        var meterHeight = this.height / 100;
+        
+        //BMIを計算する
+        var bmi = this.weight / (meterHeight * meterHeight);
+        
+        //小数点以下の桁数を、２桁にして返す
+        return bmi.toFixed(2);
+      }
+      
+      return "";
+    }
   }
 });
